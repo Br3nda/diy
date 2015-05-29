bb51b59ce43aaaa8b40dbf6df437933ea2f219ff
Brenda Wallace <brenda@rabidtech.co.nz>
2015-05-29 16:32:24 +1200

    sundry unit test stubs

---
+++ tests/unit/routes/open-badges-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:open-badges', 'Unit | Route | open badges', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
