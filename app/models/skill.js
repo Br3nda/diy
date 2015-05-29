aae2b9ceca5de23dd032cce6fa4e527463422483
Brenda Wallace <brenda@brenda.nz>
2015-05-29 22:16:50 +1200

    filled in more of the skill model

--- app/models/skill.js
+++ app/models/skill.js
@@ -6,6 +6,12 @@ let Skill = DS.Model.extend({
   url: DS.attr(),
   title: DS.attr(),
   description: DS.attr(),
+  icons: DS.attr(),
+  images: DS.attr(),
+  grammar: DS.attr(),
+  pole: DS.attr(),
+  color: DS.attr(), 
+  notes: DS.attr(),
 });
 
 Skill.reopenClass({
