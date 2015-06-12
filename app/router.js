d6cab83f81c8bc105e8d1e1099b903b6b114ab1f
Brenda Wallace <brenda@brenda.nz>
2015-06-12 20:59:18 +1200

    renamed authorize to sign-in

--- app/router.js
+++ app/router.js
@@ -9,7 +9,8 @@ Router.map(function() {
   this.route('skills');
   this.route('skill', { path: '/skill/:skill_id' });
   this.route('portfolio');
-  this.route('authorize');
+  this.route('authorize', {path: '/sign-in'});
+  
 });
 
 export default Router;
