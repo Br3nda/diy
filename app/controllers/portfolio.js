2bb66a3811c9528f7f4fb7b6a84bb4f729679628
Brenda Wallace <brenda@brenda.nz>
2015-06-12 21:45:36 +1200

    removed unused items from portfolio controller

--- app/controllers/portfolio.js
+++ app/controllers/portfolio.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isLoggedIn: function () {
-    return this.session.isLoggedIn();
-  }.property('session.token'),
-  makerId: function() {
-
-  },
-  makerUrl: function() {
-    return this.session.get('maker_url');
-  }.property('session.maker_url')
 });
