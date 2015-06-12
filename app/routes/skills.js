1b5af256f12cf1d4d1f10eda79e1a09f6d0b36eb
Brenda Wallace <brenda@brenda.nz>
2015-06-12 22:22:30 +1200

    display all the skills

--- app/routes/skills.js
+++ app/routes/skills.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return this.store.find('skill');
+    return this.store.find('skill', {limit: 500, offset: 0});
   }
 });
