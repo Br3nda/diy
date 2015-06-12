53e1a168e037fb1d6da9c96fc95c97b5d889ab7f
Brenda Wallace <brenda@brenda.nz>
2015-06-12 20:26:37 +1200

    use locationType = hash on production

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.baseURL = '/diy';
-    ENV.location = 'hash';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
