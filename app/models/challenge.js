42b39c1606ad7faec93a423a128785d58cf964b6
Brenda Wallace <brenda@brenda.nz>
2015-06-12 22:16:18 +1200

    model for challenge

--- app/models/challenge.js
+++ app/models/challenge.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  active: DS.attr(),
+  title: DS.attr(),
+  short: DS.attr(),
+  description: DS.attr(),
+  stats: DS.attr(),
+  meta: DS.attr(),
+  image: DS.attr(),
 });
