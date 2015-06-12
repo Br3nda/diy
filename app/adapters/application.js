63703725093c199411415157d4160ff2a4977d7f
Brenda Wallace <brenda@brenda.nz>
2015-06-12 22:14:28 +1200

    badges and challenges use non-ember-default URIs on the rest API

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,10 +2,15 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
     host: 'https://api.diy.org',
-    // pathForType: function (type) {
-    //   console.log(type);
-    //   return Ember.String.classify(type);
-    // },
+    pathForType: function (type) {
+      if (type == 'badge'){
+        return 'assertions';
+      }
+      if (type == 'challenge') {
+        return 'skills/skillname/challenges';
+      }
+      return Ember.String.pluralize(type);
+    },
     headers: function () {
       return {
         'x-diy-api-token': this.get('token')
