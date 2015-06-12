3e0632d3a73af67dac267c2c97db0ca0f84b878f
Brenda Wallace <brenda@brenda.nz>
2015-06-12 20:59:51 +1200

    load maker data on the portfolio route

--- app/routes/portfolio.js
+++ app/routes/portfolio.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function () {
+    this.session.loadSession();
+    return this.store.find('maker', this.session.get('maker_url'));
+  }
 });
