3b078c6c542b08be27f36c05b806336ef2128bd5
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-17 10:52:16 +1200

    reading project's extra bits.

--- app/models/clip.js
+++ app/models/clip.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  assets: DS.attr(), //hash
+  type: DS.attr(),
+  project: DS.belongsTo('project')
 });
--- app/serializers/project.js
+++ app/serializers/project.js
@@ -2,44 +2,56 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   extract: function (store, typeClass, payload /*, id, requestType*/) {
-
     var response = [];
-    var _store = store;
-
-    var processRecord = function (record) {
-      if (record.maker) {
-          _store.push('maker', record.maker);
-          record.maker = record.maker.id;
-      }
-      if (record.clips) {
-        var clips = [];
-        record.clips.forEach(function (clip) {
-          if (clip.assets) {
-            var assets = [];
-            clip.assets.forEach(function (asset) {
-              store.push(asset);
-              assets.push(asset.id);
-            });
-            clip.assets = assets;
-          }
-          _store.push(clip);
-          clips.push(clip.id);
-
-        });
-        record.clips = clips;
-      }
-    }
 
     if (Array.isArray(payload.response)){
       payload.response.forEach(function (record) {
-        response.push(processRecord(record));
+        record = processRecord(record, store);
+        response.push(record);
       });
     }
     else {
-      response.push(processRecord(payload.response));
+      response.push(processRecord(payload.response, store));
     }
 
     console.log(response);
     return response;
-  }
+  },
+
 });
+
+var  processRecord = function (record, store) {
+
+    if (record.maker) {
+      record.maker = processMaker(record.maker, store)
+    }
+
+    //If we have clips
+    if (Array.isArray(record.clips)) {
+      record.clips = processClips(record.clips, store)
+    }
+
+    return record;
+}
+
+  /**
+  Save maker into the store, and return the single integer id
+  for the project record
+  */
+var  processMaker = function (maker, store) {
+  store.push('maker', maker);
+  return maker.id;
+}
+
+var processClips = function (clips, store) {
+  var _clips = [];
+
+  clips.forEach(function(clip) {
+    //if we have assets
+    store.push('clip', clip);
+    _clips.push(clip.id);
+  });
+
+  return _clips;
+
+}
