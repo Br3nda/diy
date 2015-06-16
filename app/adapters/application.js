bb96073ff697bd8e2d2acc823944d4e925bf8939
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-16 12:26:57 +1200

    tabulation fix up

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,20 +2,25 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
     host: 'https://api.diy.org',
-    pathForType: function (type) {
-      if (type == 'badge'){
-        return 'assertions';
-      }
-      if (type == 'challenge') {
-        return 'skills/skillname/challenges';
-      }
-      return Ember.String.pluralize(type);
-    },
-    headers: function () {
-      return {
-        'x-diy-api-token': this.get('token')
-      }
-    }.property('auth.token')
+
+
+  /**
+    Override Ember's idea of how a rest adapter is laid out.
+  */
+  pathForType: function (type) {
+    if (type === 'badge'){
+      type = 'assertions';
+    }
+    return this._super(type);
+  },
+
+  /**
+  */
+  headers: function () {
+    return {
+      'x-diy-api-token': this.get('token')
+    };
+  }.property('auth.token')
 });
 
 
