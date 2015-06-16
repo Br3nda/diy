cc6ae8f2575004df7137b4799db29397f1313096
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-16 16:30:41 +1200

    project model

--- app/models/project.js
+++ app/models/project.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  achievements: DS.hasMany('achievement')
+  // maker: DS.belongsTo('maker', {async: true}),
+  achievements: DS.hasMany('achievement'),
+  stamp: DS.attr(),
+  stats: DS.attr(),
+  title: DS.attr(),
 });
