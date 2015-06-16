4cf548fff7b2edb76987ae66826583a5b7210aee
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-16 16:31:55 +1200

    sign out action working.

--- app/components/diy-top-bar.js
+++ app/components/diy-top-bar.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isSignedIn: function () {
+    return this.session.get('isSignedIn');
+  }.property('session.isSignedIn'),
+
+  actions: {
+    signOut: function () {
+      this.session.signOut();
+    }
+  }
 });
