1ca5054f1bda880d18f4b8a8f38f5a07f1222a60
Brenda Wallace <brenda@brenda.nz>
2015-06-16 23:05:52 +1200

    comments

---
+++ app/adapters/comment.js
@@ -0,0 +1,14 @@
+import ApplicationAdapter from './application';
+
+export default ApplicationAdapter.extend({
+  urlForFindQuery: function (query, modelName) {
+    //Read items from the query, so we can move to the url
+    var maker = query.maker;
+    delete query.maker;
+
+    var project = query.project;
+    delete query.project;
+
+    return '%@/%@/projects/%@/comments'.fmt(this._buildURL('makers'), maker, project);
+  },
+});
