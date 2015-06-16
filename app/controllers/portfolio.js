88694862fcff5469682da8abdd9b70be6b03d04a
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-16 14:12:26 +1200

    500 on a page

--- app/controllers/portfolio.js
+++ app/controllers/portfolio.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   achievements: function () {
-    return this.store.find('achievement', {maker: this.get('model.url')});
+    return this.store.find('achievement', {maker: this.get('model.url'), limit: 500});
   }.property('@each.achievements')
 });
