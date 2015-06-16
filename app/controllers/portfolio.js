1d3f8df42312e2d11a8a266952bfe1ec77f38add
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-16 14:02:39 +1200

    achievements (with projects) for portfolios

--- app/controllers/portfolio.js
+++ app/controllers/portfolio.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   achievements: function () {
-    var assertions = this.store.find('achievement', {maker: this.get('model.url')});
+    return this.store.find('achievement', {maker: this.get('model.url')});
   }.property('@each.achievements')
 });
--- app/models/achievement.js
+++ app/models/achievement.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  stamp: DS.attr(),
+  status: DS.attr(),
+  maker: DS.belongsTo('maker', {async: true}),
+  challenge: DS.belongsTo('challenge', {async: true}),
+  skill: DS.belongsTo('skill', {async: true}),
+  project: DS.belongsTo('project', {async: true}),
 });
