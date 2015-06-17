8f267037699f472751ddda15e3a473df3965b077
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-17 15:18:03 +1200

    comments reloading correctly

--- app/controllers/project.js
+++ app/controllers/project.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   comments: function () {
     var query = {maker: this.model.get('maker.url'), project: this.model.get('id')};
     return this.store.find('comment', query);
-  }.property('@each.model.comments'),
+  }.property('model.comments'),
 
   stats: function () {
     return this.model.get('stats');
