1582eef739edc175767180a81a3374df488c3b45
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-18 17:04:48 +1200

    Fixing up menu for small screens.

--- app/components/diy-top-bar.js
+++ app/components/diy-top-bar.js
@@ -4,13 +4,14 @@ export default Ember.Component.extend({
   isSignedIn: function () {
     return this.session.get('isSignedIn');
   }.property('session.isSignedIn'),
-
+  menuActive: false,
   actions: {
     signOut: function () {
       this.session.signOut();
     },
     showMenu: function () {
+      this.set('menuActive', !this.get('menuActive'));
+    },
 
-    }
   }
 });
