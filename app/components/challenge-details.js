1f6487f1b70f68b0c9a044eedc88a7e0368213ca
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-25 13:11:48 +1200

    only test if challenge is completed, if they are signed in.

--- app/components/challenge-details.js
+++ app/components/challenge-details.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   complete: function () {
-    return this.portfolio.hasCompletedChallenge(this.session, this.get('challenge'));
+    if (this.session.get('isSignedIn')) {
+      //read if this challenge is complete in the users' portfolio
+      return this.portfolio.hasCompletedChallenge(this.session, this.get('challenge'));
+    }
+    else {
+      return false;
+    }
   }.property('complete'),
   isSignedIn: function () {
     return this.session.get('isSignedIn');
