5e4846db805f6b660c139c1a10a8e3a845009e96
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-25 13:11:03 +1200

    some stub session tests.

--- tests/unit/services/session-test.js
+++ tests/unit/services/session-test.js
@@ -5,8 +5,40 @@ moduleFor('service:session', 'Unit | Service | session', {
   // needs: ['service:foo']
 });
 
+
+// test('sign in', function (assert) {
+//   assert.expect(1);
+
+//   var session = this.subject();
+
+//   var username = 'fakeuser';
+//   var password = 'fasspass';
+
+//   session.signIn(username, password).then(function () {
+//     assert.ok(session.get('isSignedIn'));
+//   });
+
+// });
+
+// test('can save session', function (assert) {
+//   assert.expect(1);
+//   var session = this.subject();
+//   session.saveSession({token: 'hey', id: 1, url: 'foobar'});
+// });
+
+// test('load session', function (assert) {
+//   var session = this.subject();
+//   assert.expect(1);
+// });
+
+// test('sign out', function (assert) {
+//   var session = this.subject();
+//   assert.expect(1);
+// });
+
+
 // Replace this with your real tests.
 test('it exists', function(assert) {
-  var service = this.subject();
-  assert.ok(service);
+  var session = this.subject();
+  assert.ok(session);
 });
