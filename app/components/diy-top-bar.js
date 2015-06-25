3bcf04f73b82f02485df1bfd3ff12424e4b9aa5d
Brenda Wallace <brenda@rabidtech.co.nz>
2015-06-25 13:11:31 +1200

    remove unused duplicate isSignedIn on top-bar component.

--- app/components/diy-top-bar.js
+++ app/components/diy-top-bar.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  isSignedIn: function () {
-    return this.session.get('isSignedIn');
-  }.property('session.isSignedIn'),
   menuActive: false,
+
   actions: {
     signOut: function () {
       this.session.signOut();
