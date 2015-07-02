2401035bd0033231d68bf5d1314c0c1c9401776c
Brenda Wallace <brenda@rabidtech.co.nz>
2015-07-03 09:43:57 +1200

    use the search/project api for queries.

--- app/adapters/project.js
+++ app/adapters/project.js
@@ -2,14 +2,20 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   urlForFindQuery: function (query /*, modelName*/) {
-    var maker = query.maker;
-    delete query.maker;
-
-    var url = '%@/%@/projects'.fmt(this._buildURL('makers'), maker);
     if (query.id) {
-      url += '/'  + query.id;
-      delete query.id;
+      var maker = query.maker;
+      delete query.maker;
+
+      var url = '%@/%@/projects'.fmt(this._buildURL('makers'), maker);
+      if (query.id) {
+        url += '/'  + query.id;
+        delete query.id;
+      }
+      return url;
     }
-    return url;
+    else {
+      return '%@/search/projects'.fmt(this._buildURL());
+    }
   },
+
 });
